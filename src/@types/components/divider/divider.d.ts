c6fc5bfa65251fb25345b1a4961741d29238f0e2
Sayed Riaydh <$email>
2022-03-09 14:00:29 +0600

    props-type remove

--- src/@types/components/divider/divider.d.ts
+++ src/@types/components/divider/divider.d.ts
@@ -1,7 +1,3 @@
 interface IDivider {
-  top?: boolean;
-  bottom?: boolean;
-  topSize?: number;
-  bottomSize?: number;
-  className?: string;
+  className: string;
 }
--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -1,14 +1,11 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, ReactNode, useEffect } from 'react';
 import classnames from 'classnames';
 import { MAIN_CONTENT_ID } from '@constants';
 
-const Page: FC = ({
-  children,
-  className,
-}: {
-  children: string;
-  className: string;
-}) => {
+const Page: FC<{
+  children: ReactNode;
+  className?: string;
+}> = ({ children, className = '' }) => {
   const classNames = classnames({ [className]: !!className });
 
   useEffect(() => {
