e7c7e80f395e5b6a0105d9eb0c2871ce0cfbf9d2
Sayed Riaydh <$email>
2022-03-12 13:52:38 +0600

    props-type remove

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,42 +1,44 @@
 const REQUIRED_TEXT_TID = 'VALIDATION_ERRORS.REQUIRED';
 
-const getErrorConfig = (tid, values = {}) => ({
+const getErrorConfig = (tid: string, values = {}) => ({
   tid,
   values,
 });
 
-export const minLength = (min: any) => (value) =>
+export const minLength = (min: any) => (value: number) =>
   value.toString().length >= min
     ? getErrorConfig('VALIDATION_ERRORS.MIN_LENGTH', { min })
     : null;
 export const minLength_8 = minLength(8);
 
-export const maxLength = (max) => (value = '') =>
-  (value + '').length > max
-    ? getErrorConfig('VALIDATION_ERRORS.MAX_LENGTH', { max })
-    : null;
+export const maxLength =
+  (max: number) =>
+  (value = '') =>
+    (value + '').length > max
+      ? getErrorConfig('VALIDATION_ERRORS.MAX_LENGTH', { max })
+      : null;
 export const maxLength_20 = maxLength(20);
 export const maxLength_50 = maxLength(50);
 export const maxLength_300 = maxLength(300);
 
-export const name = (value: any) =>
+export const name = (value: string) =>
   !/^([A-Za-z'-\s]*)$/.test(value)
     ? getErrorConfig('VALIDATION_ERRORS.NAME')
     : null;
 
-export const email = (value: any) =>
+export const email = (value: string) =>
   !/^(([^<>()[\]\\.,;:\s@"']+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     value,
   )
     ? getErrorConfig('VALIDATION_ERRORS.EMAIL')
     : null;
 
-export const userName = (value: any) =>
+export const userName = (value: string) =>
   !/^([A-Za-z0-9_-]*)$/.test(value)
     ? getErrorConfig('VALIDATION_ERRORS.INCORRECT_USERNAME')
     : null;
 
-export const required = (value: any) => {
+export const required = (value: string) => {
   if (!value) {
     return REQUIRED_TEXT_TID;
   }
@@ -44,7 +46,7 @@ export const required = (value: any) => {
   return null;
 };
 
-export const required_custom = (errorId: any) => (value) => {
+export const required_custom = (errorId: string) => (value: string) => {
   if (required(value)) {
     return errorId;
   }
@@ -60,7 +62,7 @@ export const required_trim = (value = '') => {
   return null;
 };
 
-export const required_custom_trim = (errorId) => (value) => {
+export const required_custom_trim = (errorId: string) => (value: string) => {
   if (required_trim(value)) {
     return errorId;
   }
@@ -68,12 +70,12 @@ export const required_custom_trim = (errorId) => (value) => {
   return null;
 };
 
-export const validator = (values, config = {}) => {
+export const validator = (values: any, config = {}) => {
   const errors = {};
 
   Object.keys(config).forEach((field) => {
     let fieldError;
-    config[field].some((rule) => {
+    config[field].some((rule: any) => {
       fieldError = rule(values[field], values);
 
       return !!fieldError;
@@ -87,7 +89,7 @@ export const validator = (values, config = {}) => {
 
 const TEXT_AREA_MAX_LENGTH = 300;
 
-export const textAreaValidation = (value) => {
+export const textAreaValidation = (value: any) => {
   if (required_trim(value)) {
     return required_trim(value);
   }
