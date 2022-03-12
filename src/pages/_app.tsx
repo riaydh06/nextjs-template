3c301d29f7289f285c10efe4503a1c1adbf92c66
Sayed Riaydh <$email>
2022-03-12 15:50:39 +0600

    template update and warning fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Provider } from 'react-redux';
+import { AppProps } from 'next/app';
 
 import { useStore } from '../helpers/store';
 import { RoutingLoader } from '@components';
@@ -7,10 +8,7 @@ import { RoutingLoader } from '@components';
 import '../helpers/i18n';
 import '../styles/styles.scss';
 
-const AppMain: FC<{ Component: any; pageProps: any }> = ({
-  Component,
-  pageProps,
-}) => {
+const AppMain: FC<AppProps> = ({ Component, pageProps }) => {
   const store = useStore(pageProps.initialReduxState);
 
   return (
