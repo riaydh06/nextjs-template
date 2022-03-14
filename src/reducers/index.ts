52bdbee47d6e7e6ff309093175f3c9904ef04780
Sayed Riaydh <$email>
2022-03-14 14:55:52 +0600

    signup reducer added

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+import signUpReducer from './signUpReducer';
 
 const rootReducer = combineReducers({
   form,
+  signUp: signUpReducer,
 });
 
 export default rootReducer;
